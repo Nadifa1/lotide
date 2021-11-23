3d4c3d408944cae45e7a1699ffba8ac94787bd7e
Nadifa Mohamed <nadifadaudmohamed@gmail.com>
2021-11-23 00:34:59 +0000

    tail exercise w/ splice

---
+++ tail.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const tail = function(array) {
+  let newArray = array.splice(1);
+  console.log(newArray);
+};
+
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result, ["Lighthouse", "Labs"]);
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);

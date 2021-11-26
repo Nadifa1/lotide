607a38ac4f90ad6fc3c11eb87ad8b45814308ec3
Nadifa Mohamed <nadifadaudmohamed@gmail.com>
2021-11-26 19:39:06 +0000

    update

--- countLetter.js
+++ countLetter.js
@@ -8,21 +8,26 @@ const assertEqual = function(actual, expected) {
 
 };
 
-//skip or don't count spaces
-//loop 
 
-
-const countLetters = function(str) {
-let unspaced = str.split(" ").join("");
-const results = {}; 
-for (let index of unspaced) { 
-    if (results[index]) {
-      results[index]++; 
+const countLetters = function(string) {
+  let unspaced = string.split(" ").join("")
+  //takes in the parameter (i.e., string) and removes the spaces " " to avoid counting it in the new object
+  const results = {}; 
+  //creating an empty/new object to put the results in
+  for (let char of unspaced) {
+    //loop through every element in the (revised) parameter
+    if (results[char]) {
+      // if the char is already in the new object (evaluates to true); add to the count/value
+      results[char] += 1;
+      //add 1 to the value 
     } else {
-    results[index] = 1; 
+      //if the char is not already in the new object (i.e., the if statement above evaluate to false); create a key with a value of 1 
+      results[char] = 1;
     }
   }
-return results; 
+  return results; 
 }
 
-console.log(countLetters("hello world"));
+console.log(countLetters("lighthouse in the house")); 
+
+
